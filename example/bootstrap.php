a1de44f6590d37e114a84bc52f0fde4409b8c3be
Babalion <48565736+Babalion@users.noreply.github.com>
2022-02-20 14:49:06 +0100

    removed unnecessary elements

--- example/bootstrap.php
+++ example/bootstrap.php
@@ -3,8 +3,6 @@
 use YOOtheme\Builder;
 use YOOtheme\Path;
 
-include_once __DIR__ . '/src/AssetsListener.php';
-include_once __DIR__ . '/src/SettingsListener.php';
 include_once __DIR__ . '/src/SourceListener.php';
 include_once __DIR__ . '/src/MyTypeProvider.php';
 include_once __DIR__ . '/src/Type/MyType.php';
@@ -12,25 +10,8 @@ include_once __DIR__ . '/src/Type/MyQueryType.php';
 
 return [
 
-    'theme' => [
-        'styles' => [
-            'components' => [
-                'my-component' => Path::get('./assets/less/my-component.less'),
-            ],
-        ],
-    ],
 
     'events' => [
-
-        // Add asset files
-        'theme.head' => [
-            AssetsListener::class => 'initHead',
-        ],
-
-        // Add settings Panels
-        'customizer.init' => [
-            SettingsListener::class => 'initCustomizer',
-        ],
 
         // Add custom demo source
         'source.init' => [
